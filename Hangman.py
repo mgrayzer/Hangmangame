043e98b13d0a3d819b2701339eb0458effc93967
mgrayzer <marlonagray@gmail.com>
2017-06-07 22:13:38 +0200

    refactor code

--- Hangman.py
+++ Hangman.py
@@ -1,10 +1,10 @@
 __author__ = 'mmwin8'
-#-*- coding: utf-8 -*-
-
+# -*- coding: utf-8 -*-
 import random as r
 import re
 import csv
 import winsound
+import webbrowser as w
 
 
 def main():
@@ -14,19 +14,19 @@ def main():
 def game():
     words = []
     with open('wordlist.csv') as csvFile:
-        fileWords = csv.reader(csvFile)
-        for rows in fileWords:
-            newWords = (', '.join(rows))
-            words.append(newWords)
+        file_words = csv.reader(csvFile)
+        for rows in file_words:
+            new_words = (', '.join(rows))
+            words.append(new_words)
 
     def randoms():
         a = r.choice(words)
         return a
-    secretWord = randoms()
-    guessesAllowed = len(secretWord) + 3
+    secret_word = randoms()
+    guesses_allowed = len(secret_word) + 3
     print('Welcome to the hangman game.')
-    print('I am thinking of a word Which is '+ str(len(secretWord)) + ' letters long')
-    print("You have got " + str(guessesAllowed) + " guesses")
+    print('I am thinking of a word Which is ' + str(len(secret_word)) + ' letters long')
+    print("You have got " + str(guesses_allowed) + " guesses")
 
     result = []
 
@@ -45,26 +45,25 @@ def game():
             result.append(b)
         return b
 
-    blank = ['_ '] * len(secretWord)
+    blank = ['_ '] * len(secret_word)
     counter = 0
-    guessesAllowed2 = guessesAllowed
-    import webbrowser as w
+    guesses_allowed2 = guesses_allowed
 
-    while counter < guessesAllowed2:
+    while counter < guesses_allowed2:
         letter()
         for steps in result[-1]:
-            if steps in secretWord:
+            if steps in secret_word:
                 print('Correct guess well done!')
-                winsound.PlaySound('C:/Users/mmwin8/Desktop/MekhiPython/correct.wav',winsound.SND_FILENAME)
+                winsound.PlaySound('correct.wav', winsound.SND_FILENAME)
             else:
                 print('Sorry Incorrect guess!')
-                winsound.PlaySound('C:/Users/mmwin8/Desktop/MekhiPython/Wrong.wav',winsound.SND_FILENAME)
+                winsound.PlaySound('Wrong.wav', winsound.SND_FILENAME)
 
         count = 0
-        for i, c in enumerate(secretWord):
+        for i, c in enumerate(secret_word):
             if c in result:
                 count += 1
-                blank.insert(count-1,c)
+                blank.insert(count-1, c)
                 blank.pop(count)
             else:
                 count += 1
@@ -74,33 +73,33 @@ def game():
 
         print("This is your progress " + result2)
 
-        if result2 == secretWord:
+        if result2 == secret_word:
             print("You have gotten it huraaaaaaaaaaay!!")
-            winsound.PlaySound('C:/Users/mmwin8/Desktop/MekhiPython/Applause.wav',winsound.SND_FILENAME)
+            winsound.PlaySound('Applause.wav', winsound.SND_FILENAME)
             break
-        if result[-1] not in secretWord:
-            guessesAllowed2 += -1
+        if result[-1] not in secret_word:
+            guesses_allowed2 += -1
 
-        if guessesAllowed2 > 1:
-            print("You have got " + (str(guessesAllowed2)) + " Guesses left")
+        if guesses_allowed2 > 1:
+            print("You have got " + (str(guesses_allowed2)) + " Guesses left")
 
-        elif guessesAllowed2 == 0:
-            print("You are dead the correct word is " + secretWord)
+        elif guesses_allowed2 == 0:
+            print("You are dead the correct word is " + secret_word)
             w.open('https://em.wattpad.com/18d21d96bcc10003b9371cd\n'
                    'a793d061779447d40/687474703a2f2f322e62702e626c6\n'
                    'f6773706f742e636f6d2f2d3550\n'
                    '516d6a685a61495f4d2f554161773833696738474\n'
                    '92f41414141414141414677632f7a387a4d5755723034334d2\n'
                    'f73313630302f68616e676d616e362e676966')
-            winsound.PlaySound('C:/Users/mmwin8/Desktop/MekhiPython/dead.wav',winsound.SND_FILENAME)
+            winsound.PlaySound('dead.wav', winsound.SND_FILENAME)
 
         else:
-            print("You have got " + (str(guessesAllowed2)) + " Guess left")
-    tryAgain = input("would you like to try again y/n?")
-    if tryAgain == "y":
+            print("You have got " + (str(guesses_allowed2)) + " Guess left")
+    try_again = input("would you like to try again y/n?")
+    if try_again == "y":
         game()
     else:
         print("good bye")
 
-
-main()
+if __name__ == '__main__':
+    main()
